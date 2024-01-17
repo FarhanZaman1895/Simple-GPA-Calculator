7e55f712682333709d718ab76af366aeeeff7855
Farhan Zaman <farhanzaman1895@gmail.com>
2024-01-17 09:37:18 -0700

    Restructered program to have a grades class and a menu

--- gpacalc.cpp
+++ gpacalc.cpp
@@ -1,98 +1,193 @@
 // Includes
-#include <iomanip>
 #include <iostream>
 #include <string>
+#include <vector>
 
-// Predefining Functions
-float gradeInput();
-float gradeToGpa(std::string letter);
-float average(float grades[], int gradec);
+// Data Types
+class Grade
+{
+    // Properties
+private:
+    std::string courseName;
+    std::string letterGrade;
+    float numberGrade;
+    float units;
+    float gradeUnits;
 
-// Main Function
-int main (int argc, char *argv[]) {
-
-    // Number of Grades
-    int numberOfGrades;
-    std::cout << "How many grades would you like to calculate for: ";
-    std::cin >> numberOfGrades;
-
-    // Initialize Array
-    float gradesArr[numberOfGrades];
-
-    // Get Grade Values
-    for (int i = 0; i < numberOfGrades; i++) {
-        std::cout << "Grade #" << i + 1 << "\n";
-        float gpaVal {gradeInput()};
-        gradesArr[i] = gpaVal;
+    // Public Methods
+public:
+    Grade(std::string courseName, std::string letterGrade, float units):
+        courseName(courseName),
+        letterGrade(letterGrade),
+        units(units)
+    {
+        numberGrade = gradeToNum(letterGrade);
+        gradeUnits = units * numberGrade;
     }
 
-    // Average
-    float ave = average(gradesArr, numberOfGrades);
+    void displayGrade()
+    {
+        std::cout << "Course: " << courseName << ", Letter Grade: "
+            << letterGrade << ", Number Grade: " << 
+            numberGrade << ", Units: " << units << "\n";
+    }
 
-    // Final Output
-    std::cout << "Your GPA is " << std::fixed << std::setprecision(1) << ave << std::endl;
+    void changeGrade(std::string newGrade)
+    {
+        letterGrade = newGrade;
+        numberGrade = gradeToNum(newGrade);
+    }
+
+    float getNumberGrade()
+    {
+        return numberGrade;
+    }
+
+private:
+    float gradeToNum(std::string letter)
+    {
+
+        float gpaVal;
+
+        // Idk how to make this more efficient
+        if (letter.compare("A") == 0 || letter.compare("A+") == 0)
+        {
+            gpaVal = 4;
+        } else if (letter.compare("A-") == 0)
+        {
+            gpaVal = 3.7;
+        } else if (letter.compare("B+") == 0)
+        {
+            gpaVal = 3.3;
+        } else if (letter.compare("B") == 0)
+        {
+            gpaVal = 3;
+        } else if (letter.compare("B-") == 0)
+        {
+            gpaVal = 2.7;
+        } else if (letter.compare("C+") == 0)
+        {
+            gpaVal = 2.3;
+        } else if (letter.compare("C") == 0)
+        {
+            gpaVal = 2;
+        } else if (letter.compare("C-") == 0)
+        {
+            gpaVal = 1.7;
+        } else if (letter.compare("D+") == 0)
+        {
+            gpaVal = 1.3;
+        } else if (letter.compare("D") == 0)
+        {
+            gpaVal = 1;
+        } else if (letter.compare("F") == 0)
+        {
+            gpaVal = 0;
+        } else
+        {
+            gpaVal = -1;
+        }
+
+        return gpaVal;
+    }
+};
+
+// Predefining Functions
+int mainMenu();
+int menuInput(int min, int max);
+void displayGrades(std::vector<Grade> gradeArr);
+void addGrade(std::vector<Grade> gradeArr);
+
+// Main Function
+int main(int argc, char *argv[])
+{
+
+    std::cout << "Welcome to a GPA Calculating Program\n";
+    int choice;
+
+    std::vector<Grade> gradeArr;
+
+    do
+    {
+        std::cout << std::endl;
+        choice = mainMenu();
+        if (choice == 1)
+        {
+            displayGrades(gradeArr);
+        } else if (choice == 2)
+        {
+            addGrade(gradeArr);
+        }
+
+    } while (choice != 0);
 
     return 0;
 }
 
-// Side Functions
-float gradeInput() {
+int mainMenu()
+{
 
-    float gpaVal {-1};
-    std::string letter;
+    // Print Menu
+    std::cout << "0. Exit Program\n"
+        << "1. Display Classes\n" 
+        << "2. Add Class\n" 
+        << "3. Remove Class\n"
+        << "4. Edit Class Grade\n"
+        << "5. Calculate Average\n";
 
-    do {
-        std::cout << "Please enter a valid grade: ";
-        std::cin >> letter;
-        gpaVal = gradeToGpa(letter);
-    } while (gpaVal == -1);
-
-    return gpaVal;
+    return menuInput(0, 5);
 }
 
-float gradeToGpa(std::string letter) {
+int menuInput(int min, int max)
+{
 
-    float gpaVal;
+    int choiceInt;
+    bool loop {true};
 
-    // Idk how to make this more efficient
-    if (letter.compare("A") == 0 || letter.compare("A+") == 0) {
-        gpaVal = 4;
-    } else if (letter.compare("A-") == 0) {
-        gpaVal = 3.7;
-    } else if (letter.compare("B+") == 0) {
-        gpaVal = 3.3;
-    } else if (letter.compare("B") == 0) {
-        gpaVal = 3;
-    } else if (letter.compare("B-") == 0) {
-        gpaVal = 2.7;
-    } else if (letter.compare("C+") == 0) {
-        gpaVal = 2.3;
-    } else if (letter.compare("C") == 0) {
-        gpaVal = 2;
-    } else if (letter.compare("C-") == 0) {
-        gpaVal = 1.7;
-    } else if (letter.compare("D+") == 0) {
-        gpaVal = 1.3;
-    } else if (letter.compare("D") == 0) {
-        gpaVal = 1;
-    } else if (letter.compare("F") == 0) {
-        gpaVal = 0;
-    } else {
-        gpaVal = -1;
+    while (loop)
+    {
+        std::cout << "Enter a valid option: ";
+        std::cin >> choiceInt;
+        if (!std::cin)
+        {
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
+            continue;
+        } else if (choiceInt >= min && choiceInt <= max)
+        {
+            std::cout << std::endl;
+            loop = false;
+        }
     }
 
-    return gpaVal;
+    return choiceInt;
 }
 
-float average(float *grades, int gradec) {
-
-    float ave {0};
-
-    for (int i = 0; i < gradec; i++) {
-        ave += grades[i];
+void displayGrades(std::vector<Grade> gradeArr)
+{
+    if (gradeArr.size() > 0)
+    {
+        for (int i {0}; i < gradeArr.size(); i++)
+        {
+            gradeArr[i].displayGrade();
+        }
+    } else
+    {
+        std::cout << "There are no courses" << "\n";
     }
+}
 
-    ave /= gradec;
+void addGrade(std::vector<Grade> gradeArr)
+{
+    std::string courseName, letterGrade;
+    float units;
+    std::cout << "Please add a course name: ";
+    std::cin >> courseName;
+    std::cout << "Please add letter grade: ";
+    std::cin >> letterGrade;
+    std::cout << "Please add course units: ";
+    std::cin >> units;
 
-    return ave;
+    Grade tempGrade {courseName, letterGrade, units};
+    gradeArr.push_back(tempGrade);
 }
